95421d308cc83242f71d461e5dd56a353ee866bb
YannickMontes <montes.yannick@gmail.com>
2017-07-29 18:37:44 +0200

    Implement basic movement (sliding on X)

--- Assets/Warning/Scripts/EnnemyMovement.cs
+++ Assets/Warning/Scripts/EnnemyMovement.cs
@@ -6,14 +6,23 @@ public class EnnemyMovement : MonoBehaviour {
 
     private Transform player;
 
-    public bool isChasing;
+    public int moveSpeed;
+
+    private bool isChasing;
+
+    private int direction;
+
+    private Vector3 destination;
 
 	// Use this for initialization
 	void Start ()
     {
         isChasing = false;
+        moveSpeed = 5;
         this.player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-	}
+        this.direction = 1;
+        this.destination = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -25,23 +34,49 @@ public class EnnemyMovement : MonoBehaviour {
 
     private void Move()
     {
+        Vector3 moving;
         if (isChasing)
-            Chase();
+            moving = Chase();
         else
-            MoveNormaly();
+            moving = MoveNormaly();
+        if (Vector3.Distance(player.position, this.transform.position) > 2.0f)
+            this.GetComponent<Rigidbody>().MovePosition(transform.position + moving * Time.deltaTime * moveSpeed);
     }
 
-    private void MoveNormaly()
+    private Vector3 MoveNormaly()
     {
-
+        if (Vector3.Distance(this.transform.position, this.destination) < 1f && direction !=0)
+        {
+            StartCoroutine(WaitASecond());
+        }
+        if (direction == 0)
+        {
+            return Vector3.zero;
+        }
+        else if (direction == 1)
+        {
+            return Vector3.right;
+        }
+        else if (direction == -1)
+        {
+            return Vector3.left;
+        }
+        return Vector3.zero;
     }
 
-    private void Chase()
+    public IEnumerator WaitASecond()
+    {
+        int newDirection = this.direction*(-1);
+        this.destination = new Vector3(this.transform.position.x + (10 * newDirection), this.transform.position.y, this.transform.position.z);
+        this.direction = 0;
+        yield return new WaitForSeconds(1);
+        this.direction = newDirection;
+    }
+
+    private Vector3 Chase()
     {
         Vector3 playerDirection = player.position - this.transform.position;
-        if (Vector3.Distance(player.position, this.transform.position) > 2.0f)
-            this.GetComponent<Rigidbody>().MovePosition(transform.position + playerDirection * Time.deltaTime);
-            //this.transform.Translate(playerDirection * Time.deltaTime);
+        return playerDirection.normalized;
     }
 
     private void CheckForPlayer()
