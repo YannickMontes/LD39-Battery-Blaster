f3e7d9287756303fe09c80040f750e5f1d2e269c
YannickMontes <montes.yannick@gmail.com>
2017-07-29 20:36:10 +0200

    Create ennemyspawner script

---
+++ Assets/Warning/Scripts/EnnemySpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemySpawner : MonoBehaviour {
+
+    public GameObject ennemy;
+    public bool canGenerate;
+    public int maxGeneration;
+    public int currentGeneration;
+
+	// Use this for initialization
+	void Start ()
+    {
+        InvokeRepeating("Spawn", 0.0f, 5.0f);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void Spawn()
+    {
+        if (canGenerate)
+        {
+            if (currentGeneration >= maxGeneration)
+            {
+                CancelInvoke();
+            }
+            else
+            {
+                GameObject en = Instantiate(ennemy);
+                en.GetComponent<EnnemyMovement>().onlyChase = true;
+                en.transform.position = this.transform.position;
+            }
+            currentGeneration++;
+        }
+    }
+
+    public void TurnGenerateOn()
+    {
+        this.canGenerate = true;
+    }
+}
