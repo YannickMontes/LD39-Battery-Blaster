ed50c3d7471533d93478effcb0b61b13cb41d8dc
YannickMontes <montes.yannick@gmail.com>
2017-07-29 16:49:37 +0200

    Remove main scene & add ennemy movement

---
+++ Assets/Warning/Scripts/EnnemyMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyMovement : MonoBehaviour {
+
+    private Transform player;
+
+    public bool isChasing;
+
+	// Use this for initialization
+	void Start ()
+    {
+        isChasing = false;
+        this.player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        CheckForPlayer();
+
+        Move();
+    }
+
+    private void Move()
+    {
+        if (isChasing)
+            Chase();
+        else
+            MoveNormaly();
+    }
+
+    private void MoveNormaly()
+    {
+
+    }
+
+    private void Chase()
+    {
+
+    }
+
+    private void CheckForPlayer()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(this.transform.position, 20.0f);
+        int i = 0;
+        while (i < hitColliders.Length)
+        {
+            if (hitColliders[i].tag == "Player")
+            {
+                isChasing = true;
+                return;
+            }
+        }
+        isChasing = false;
+    }
+}
