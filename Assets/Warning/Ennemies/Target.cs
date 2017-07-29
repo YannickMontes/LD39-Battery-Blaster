a977076a8fae82434dc301f71e98501f9a4c4ef7
PapyKent <kentdu38@gmail.com>
2017-07-29 16:28:49 +0200

    ennemies + weapons [blaster]

---
+++ Assets/Warning/Ennemies/Target.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+	public float health = 50f;
+
+	public void TakeDamage(float amount){
+		health -= amount;
+		if (health <= 0f) {
+			Die ();
+		}
+	}
+
+	void Die(){
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Warning/Weapons/ShootManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootManager : MonoBehaviour {
+
+	public float damage = 10f;
+	public float range = 100f;
+	public ParticleSystem flash;
+
+	public Camera fpsCamera;
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1")){
+			Shoot();
+		}
+			
+	}
+
+	void Shoot(){
+		flash.Play ();
+
+
+		RaycastHit hitPoint;
+		if (Physics.Raycast (fpsCamera.transform.position, fpsCamera.transform.forward, out hitPoint, range)) {
+			Debug.Log (hitPoint.transform.name);
+
+			Target target = hitPoint.transform.GetComponent<Target> ();
+			if (target != null) {
+				target.TakeDamage (damage);
+			}
+		}
+
+	}
+
+
+}
