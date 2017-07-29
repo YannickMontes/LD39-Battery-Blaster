9dfaaaeb14f49f3139a0b857a50673bcbaa039d9
YannickMontes <montes.yannick@gmail.com>
2017-07-29 20:36:36 +0200

    Create spawn trigger script
    Enable given ennemy spawner when colliding

---
+++ Assets/Warning/Scripts/SpawnTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTrigger : MonoBehaviour {
+
+    public EnnemySpawner[] ennemySpawner;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            for (int i = 0; i < ennemySpawner.Length; i++)
+            {
+                ennemySpawner[i].TurnGenerateOn();
+            }
+        }
+    }
+}
