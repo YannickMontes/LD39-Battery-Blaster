9db4aaacd9af87ad8fde24122c93039793e03c5f
PapyKent <kentdu38@gmail.com>
2017-07-30 03:10:11 +0200

    blaster

---
+++ Assets/Warning/Scripts/GrosseBlasterShootManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrosseBlasterShootManager : MonoBehaviour {
+
+	float downTime, upTime, pressTime = 0;
+	public float CostPerSec = 10.0f;
+	public ParticleSystem flashCharge;
+	public ParticleSystem flashShoot;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown("Fire3")  && Terminator.GetTerminator().energy.CurrentValue > 0) {
+			downTime = Time.time;
+			StartCoroutine(StartShooting());
+		}
+			
+
+
+		if (Input.GetButtonUp("Fire3")) {
+			flashCharge.Stop ();
+			upTime = Time.time;
+			pressTime = upTime - downTime;
+			Shoot ();
+		}
+		
+	}
+
+	IEnumerator StartShooting() {
+		while (Input.GetButton("Fire3") && Terminator.GetTerminator().energy.CurrentValue > 0) {
+			flashCharge.Play ();
+			Terminator.GetTerminator ().DecreaseEnergy (CostPerSec);
+			yield return new WaitForSeconds(1.0f);
+		}
+	}
+
+	void Shoot(){
+		flashShoot.Play ();
+		Invoke("Stop", pressTime);
+	}
+		
+
+	void Stop(){
+		flashShoot.Stop ();
+	}
+}
