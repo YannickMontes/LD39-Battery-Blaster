c537ae8d650d40757d4dbd0f8ca30e8c3fbbde8c
Vincent Delannoy <volgix@yahoo.fr>
2017-07-30 12:10:13 +0200

    Asset PostProcessing package

---
+++ Assets/Warning/Scripts/UI/MonitorEffect.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonitorEffect : MonoBehaviour {
+
+
+    private void OnGUI() {
+        
+    }
+
+}
