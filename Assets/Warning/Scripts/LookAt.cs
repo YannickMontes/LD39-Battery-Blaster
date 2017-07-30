19beab138a10ae84459e585c1d8c059bc9b32517
Vincent Delannoy <volgix@yahoo.fr>
2017-07-30 11:46:01 +0200

    SkullFurher asset and 4 shared materials

---
+++ Assets/Warning/Scripts/LookAt.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour {
+
+    public bool lookAtMainCamera;
+    public GameObject target;
+
+	void Update () {
+        if (lookAtMainCamera) {
+            this.transform.LookAt(Camera.main.transform);
+        } else {
+            this.transform.LookAt(target.transform);
+        }
+	}
+}
