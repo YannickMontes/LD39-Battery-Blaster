5bcdfc7ce714fa8cdfad900f0afe3df1c613bce5
YannickMontes <montes.yannick@gmail.com>
2017-07-31 00:23:28 +0200

    Add scripts to rotate/translate objectives & put objectives on scene

---
+++ Assets/Warning/Scripts/Tools/UpDownTransform.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpDownTransform : MonoBehaviour {
+
+    public float limitUp;
+    public float limitDown;
+    public bool up;
+
+	// Use this for initialization
+	void Start () {
+        up = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (up)
+        {
+            if (this.transform.position.y < this.limitUp)
+            {
+                this.transform.Translate(Vector3.forward * Time.deltaTime);
+            }
+            else
+            {
+                this.up = false;
+            }
+        }
+        else
+        {
+            if (this.transform.position.y > this.limitDown)
+            {
+                this.transform.Translate(Vector3.back * Time.deltaTime);
+            }
+            else
+            {
+                this.up = true;
+            }
+        }
+	}
+}
