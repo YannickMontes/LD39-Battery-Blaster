3941afa88d64db493af3fb3a5e1075286a525b79
YannickMontes <montes.yannick@gmail.com>
2017-07-31 19:41:40 +0200

    BatteryColor script

---
+++ Assets/Warning/Scripts/UI/BatteryColor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BatteryColor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Terminator.GetTerminator().energy.CurrentValue <= 30)
+        {
+            this.GetComponent<Image>().color = new Color(1.0f, 0.0f, 0.0f);
+        }
+        else
+        {
+            this.GetComponent<Image>().color = new Color(0.0f, 1.0f, 0.0f);
+        }
+    }
+}
