bc341f7a944b04188be4edb43049aa543b5c767f
YannickMontes <montes.yannick@gmail.com>
2017-07-31 19:36:34 +0200

    Add low battery & change scene

--- Assets/Warning/Scripts/Player/PlayerInput.cs
+++ Assets/Warning/Scripts/Player/PlayerInput.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerInput : MonoBehaviour {
 
     public bool gIsDown;
     public float rollingFaceLimit;
     public float energyGain;
+    public Image key1;
+    public Image key2;
+    public Color lowAlpha = new Color(1.0f, 1.0f, 1.0f, 0.25f);
+    public Color highAlpha = new Color(1.0f, 1.0f, 1.0f, 1.0f);
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         gIsDown = false;
         InvokeRepeating("CheckRollingFaceRate", 0.0f, rollingFaceLimit);
+        key1.color = highAlpha;
+        key2.color = lowAlpha;
     }
 	
 	// Update is called once per frame
@@ -21,11 +28,15 @@ public class PlayerInput : MonoBehaviour {
         if (!gIsDown && Input.GetKeyDown(KeyCode.G))
         {
             gIsDown = true;
+            key1.color = lowAlpha;
+            key2.color = highAlpha;
         }
         else if (gIsDown && Input.GetKeyDown(KeyCode.H))
         {
             gIsDown = false;
             this.gameObject.GetComponent<Terminator>().energy.CurrentValue += energyGain;
+            key1.color = highAlpha;
+            key2.color = lowAlpha;
         }
 
         // CHEAT TO REMOVE
@@ -38,7 +49,11 @@ public class PlayerInput : MonoBehaviour {
 
     private void CheckRollingFaceRate()
     {
-        if(gIsDown)
+        if (gIsDown)
+        {
             gIsDown = false;
+            key1.color = highAlpha;
+            key2.color = lowAlpha;
+        }
     }
 }
