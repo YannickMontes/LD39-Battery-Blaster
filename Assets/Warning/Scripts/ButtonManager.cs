6de5b766a7f68b7eb4f4897f5c654fa183733e37
PapyKent <kentdu38@gmail.com>
2017-07-31 18:33:53 +0200

    menu update

--- Assets/Warning/Scripts/ButtonManager.cs
+++ Assets/Warning/Scripts/ButtonManager.cs
@@ -4,10 +4,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour {
-	public AudioClip clic;
 
 	public void newGameButton(string newGameLevel){
-		SoundManager.instance.RandomizeSfx (clic);
 		SceneManager.LoadScene (newGameLevel);
 	}
 		
